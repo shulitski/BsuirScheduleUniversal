9c67f5a2f6934a7c6e9b9a92a234ee86ef59bab5
Дмитрий <dmitrii-1997@outlook.com>
2018-11-04 13:41:00 +0300

    Main page refactoring

--- BsuirScheduleUniversal/MainPage.xaml.cs
+++ BsuirScheduleUniversal/MainPage.xaml.cs
@@ -27,76 +27,12 @@ using Windows.UI.Popups;
 
 namespace BsuirScheduleUniversal
 {
-    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    public sealed partial class MainPage : Page
     {
-        private static ApplicationDataContainer LocalSettings => ApplicationData.Current.LocalSettings;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
+        private FullScheduleVM VM => (FullScheduleVM) DataContext;
         private bool _selectionLocked = false;
-        private bool _isBusy = false;
-        private DateTime? _beginDate;
-        private DateTime? _endDate;
-        public ObservableCollection<DayScheduleVM> Schedule;
-        public Visibility LoadMoreVisibility => (Schedule != null && Schedule.Count > 0 && !IsFullSchedule) ? Visibility.Visible : Visibility.Collapsed;
 
-        public bool IsBusy
-        {
-            get => _isBusy;
-            set
-            {
-                _isBusy = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        public bool IsFullSchedule
-        {
-            get => (LocalSettings.Values["IsFullSchedule"] as bool?) ?? false;
-            set
-            {
-                LocalSettings.Values["IsFullSchedule"] = (bool?)value;
-                Reload();
-            }
-        }
-
-        private int CheckedSubgroup
-        {
-            get => (LocalSettings.Values["checkedSubgroup"] as int?) ?? 0;
-            set
-            {
-                LocalSettings.Values["checkedSubgroup"] = (int?) value;
-                Reload();
-            }
-        }
-
-        private bool IsSubgroup0 => CheckedSubgroup == 0;
-        private bool IsSubgroup1 => CheckedSubgroup == 1;
-        private bool IsSubgroup2 => CheckedSubgroup == 2;
-        private int _currentDayIndex;
-
-        private string _selectedGroup = null;
-        public string SelectedGroup
-        {
-            get => _selectedGroup ?? (LocalSettings.Values["selectedGroup"] as string);
-            set
-            {
-                SetSelectedGroup(value);
-            }
-        }
-
-        private async void SetSelectedGroup(string value)
-        {
-            _selectedGroup = value;
-            NotifyPropertyChanged();
-            await Reload();
-            LocalSettings.Values["selectedGroup"] = _selectedGroup;
-        }
-
+        
         public MainPage()
         {
             InitializeComponent();
@@ -104,88 +40,17 @@ namespace BsuirScheduleUniversal
             FillGroupCombobox();
         }
 
-        private async Task<ObservableCollection<DayScheduleVM>> LoadSchedule()
+        private void SubgroupChecked(object sender, RoutedEventArgs e)
         {
-            var schedule = new ObservableCollection<DayScheduleVM>();
-            DateTime day = DateTime.Today;
-            for (int i = 0; i < 7; i++)
-            {
-                day = day.AddDays(-1);
-                _currentDayIndex++;
-                if (day.DayOfWeek == DayOfWeek.Monday)
-                    break;
-            }
-
-            _beginDate = day;
-
-            for (; day < _beginDate.Value.AddDays(30) || day.DayOfWeek != DayOfWeek.Monday; day = day.AddDays(1))
-            {
-                schedule.Add(await DayScheduleVM.Create(SelectedGroup, day, CheckedSubgroup));
-            }
-
-            _endDate = day.AddDays(-1);
-            return schedule;
-        }
-
-        private async Task<ObservableCollection<DayScheduleVM>> LoadFullSchedule()
-        {
-            var schedule = new ObservableCollection<DayScheduleVM>();
-            
-            for (var day = DayOfWeek.Monday; day != DayOfWeek.Sunday; day = (DayOfWeek)((int)(day + 1) % 7))
-            {
-                schedule.Add(await DayScheduleVM.CreateFull(SelectedGroup, day, CheckedSubgroup));
-            }
-            return schedule;
-        }
-
-        private async void OnScheduleUpdated(ScheduleResponse schedule)
-        {
-            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-                async () =>
-                {
-                    Reload();
-                    var dialog = new MessageDialog("Schedule updated");
-                    await dialog.ShowAsync();
-                }
-            );
+            var subgroup = int.Parse((string)((RadioButton) sender).Tag);
+            VM.SubgroupChecked(subgroup);
         }
 
         private async Task Reload()
         {
             if (ScheduleGridView == null) return;
-            if (SelectedGroup == null) return;
-            IsBusy = true;
-
-            try
-            {
-                Schedule = null;
-
-                if(IsFullSchedule)
-                    Schedule = await LoadFullSchedule();
-                else
-                    Schedule = await LoadSchedule();
-                Loader.AddScheduleUpdateListener(OnScheduleUpdated, SelectedGroup);
-            }
-            catch (ScheduleLoadingException e)
-            {
-                if(LocalSettings.Values.ContainsKey("selectedGroup"))
-                    SelectedGroup = LocalSettings.Values["selectedGroup"] as string; // Revert selected group
-                return;
-            }
-            catch (Exception e)
-            {
-                // ignored
-            }
-
+            await VM.Reload();
             FillGroupCombobox();
-            NotifyPropertyChanged("Schedule");
-            NotifyPropertyChanged("LoadMoreVisibility");
-            IsBusy = false;
-        }
-
-        private void SubgroupChecked(object sender, RoutedEventArgs e)
-        {
-            CheckedSubgroup = int.Parse((string)((RadioButton) sender).Tag);
         }
 
         private void FillGroupCombobox()
@@ -196,22 +61,23 @@ namespace BsuirScheduleUniversal
             {
                 foreach (var group in Loader.CachedGroupsArray)
                 {
-                    var panel = new StackPanel();
-                    panel.Orientation = Orientation.Horizontal;
+                    var panel = new StackPanel {Orientation = Orientation.Horizontal};
 
-                    var button = new Button();
-                    button.Content = "X";
-                    button.Margin = new Thickness(0, 0, 10, 0);
+                    var button = new Button
+                    {
+                        Content = "X",
+                        Margin = new Thickness(0, 0, 10, 0)
+                    };
+
                     button.Click += (s, e) => DeleteGroup(group);
                     panel.Children.Add(button);
 
-                    var name = new TextBlock();
-                    name.Text = group;
+                    var name = new TextBlock {Text = group};
                     panel.Children.Add(name);
 
 
                     GroupComboBox.Items.Add(panel);
-                    if (group == SelectedGroup)
+                    if (group == VM.SelectedGroup)
                         GroupComboBox.SelectedItem = group;
                 }
             }
@@ -226,26 +92,13 @@ namespace BsuirScheduleUniversal
 
             if(GroupComboBox.SelectedItem == null) return;
 
-            var panel = GroupComboBox.SelectedItem as StackPanel;
-            string value = null;
-            if (panel == null) 
-                value = GroupComboBox.SelectedItem as string;
-            else
+            string value;
+            if (GroupComboBox.SelectedItem is StackPanel panel)
                 value = ((panel.Children[1] ?? panel.Children[0]) as TextBlock)?.Text;
-            if (value == null) return;
-
-            if (value == "Load group...")
-            {
-                AddGroupDialog dlg = new AddGroupDialog();
-                await dlg.ShowAsync();
-                if (dlg.Value == null) return;
-
-                SelectedGroup = dlg.Value;
-            }
             else
-            {
-                SelectedGroup = value;
-            }
+                value = GroupComboBox.SelectedItem as string;
+
+            await VM.GroupSelected(value);
         }
 
         private void DeleteGroup(string group)
@@ -256,32 +109,22 @@ namespace BsuirScheduleUniversal
 
         private void ChartButton_OnClick(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(Chart));
+            Frame.Navigate(typeof(Chart));
         }
 
         private void DayScheduleControl_OnPairSelected(PairVM obj)
         {
-            this.Frame.Navigate(typeof(PairPage), obj);
+            Frame.Navigate(typeof(PairPage), obj);
         }
 
         private async void LoadUpClick(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 7; i++)
-            {
-                _beginDate = _beginDate.Value.AddDays(-1);
-                Schedule.Insert(0, await DayScheduleVM.Create(SelectedGroup, _beginDate.Value, CheckedSubgroup));
-            }
-            NotifyPropertyChanged("Schedule");
+            await VM.LoadMore(false);
         }
 
         private async void LoadDownClick(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 7; i++)
-            {
-                _endDate = _endDate.Value.AddDays(1);
-                Schedule.Add(await DayScheduleVM.Create(SelectedGroup, _endDate.Value, CheckedSubgroup));
-            }
-            NotifyPropertyChanged("Schedule");
+            await VM.LoadMore(true);
         }
 
         private void DayScheduleControl_PairDeleted()
---
+++ BsuirScheduleUniversal/ViewModels/FullScheduleVM.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using BsuirScheduleLib.BsuirApi.Schedule;
+
+namespace BsuirScheduleUniversal.ViewModels
+{
+    public class FullScheduleVM : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Fields
+        private bool _isBusy;
+        private int _currentDayIndex;
+        private DateTime? _beginDate;
+        private DateTime? _endDate;
+        private string _selectedGroup;
+        #endregion
+
+        #region Properties
+
+        private static ApplicationDataContainer LocalSettings => ApplicationData.Current.LocalSettings;
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool IsFullSchedule
+        {
+            get => (LocalSettings.Values["IsFullSchedule"] as bool?) ?? false;
+            set
+            {
+                LocalSettings.Values["IsFullSchedule"] = (bool?)value;
+                Reload();
+            }
+        }
+
+        public ObservableCollection<DayScheduleVM> Schedule { get; set; }
+        public Visibility LoadMoreVisibility => (Schedule != null && Schedule.Count > 0 && !IsFullSchedule) ? Visibility.Visible : Visibility.Collapsed;
+
+        public string SelectedGroup
+        {
+            get => _selectedGroup ?? (LocalSettings.Values["selectedGroup"] as string);
+            set => SetSelectedGroup(value);
+        }
+
+        private int CheckedSubgroup
+        {
+            get => (LocalSettings.Values["checkedSubgroup"] as int?) ?? 0;
+            set
+            {
+                LocalSettings.Values["checkedSubgroup"] = (int?)value;
+                Reload();
+            }
+        }
+
+        public bool IsSubgroup0 => CheckedSubgroup == 0;
+        public bool IsSubgroup1 => CheckedSubgroup == 1;
+        public bool IsSubgroup2 => CheckedSubgroup == 2;
+
+        #endregion
+
+        #region Methods
+
+        public FullScheduleVM()
+        {
+
+        }
+
+        public void SubgroupChecked(int subgroup)
+        {
+            CheckedSubgroup = subgroup;
+        }
+
+        private async void SetSelectedGroup(string value)
+        {
+            _selectedGroup = value;
+            NotifyPropertyChanged();
+            await Reload();
+            LocalSettings.Values["selectedGroup"] = _selectedGroup;
+        }
+
+        private async Task<ObservableCollection<DayScheduleVM>> LoadSchedule()
+        {
+            var schedule = new ObservableCollection<DayScheduleVM>();
+            DateTime day = DateTime.Today;
+            for (int i = 0; i < 7; i++)
+            {
+                day = day.AddDays(-1);
+                _currentDayIndex++;
+                if (day.DayOfWeek == DayOfWeek.Monday)
+                    break;
+            }
+
+            _beginDate = day;
+
+            for (; day < _beginDate.Value.AddDays(30) || day.DayOfWeek != DayOfWeek.Monday; day = day.AddDays(1))
+            {
+                schedule.Add(await DayScheduleVM.Create(SelectedGroup, day, CheckedSubgroup));
+            }
+
+            _endDate = day.AddDays(-1);
+            return schedule;
+        }
+
+        private async Task<ObservableCollection<DayScheduleVM>> LoadFullSchedule()
+        {
+            var schedule = new ObservableCollection<DayScheduleVM>();
+
+            for (var day = DayOfWeek.Monday; day != DayOfWeek.Sunday; day = (DayOfWeek)((int)(day + 1) % 7))
+            {
+                schedule.Add(await DayScheduleVM.CreateFull(SelectedGroup, day, CheckedSubgroup));
+            }
+            return schedule;
+        }
+
+        private async void OnScheduleUpdated(ScheduleResponse schedule)
+        {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                async () =>
+                {
+                    Reload();
+                    var dialog = new MessageDialog("Schedule updated");
+                    await dialog.ShowAsync();
+                }
+            );
+        }
+
+        public async Task Reload()
+        {
+            //if (ScheduleGridView == null) return;
+            if (SelectedGroup == null) return;
+            IsBusy = true;
+
+            try
+            {
+                Schedule = null;
+
+                if (IsFullSchedule)
+                    Schedule = await LoadFullSchedule();
+                else
+                    Schedule = await LoadSchedule();
+                Loader.AddScheduleUpdateListener(OnScheduleUpdated, SelectedGroup);
+            }
+            catch (ScheduleLoadingException e)
+            {
+                if (LocalSettings.Values.ContainsKey("selectedGroup"))
+                    SelectedGroup = LocalSettings.Values["selectedGroup"] as string; // Revert selected group
+                return;
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+
+            //FillGroupCombobox();
+            NotifyPropertyChanged("Schedule");
+            NotifyPropertyChanged("LoadMoreVisibility");
+            IsBusy = false;
+        }
+
+        public async Task LoadMore(bool down)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                DateTime date;
+                date = down 
+                    ? (_endDate   =   _endDate.Value.AddDays( 1)).Value 
+                    : (_beginDate = _beginDate.Value.AddDays(-1)).Value;
+
+                Schedule.Add(await DayScheduleVM.Create(SelectedGroup, date, CheckedSubgroup));
+            }
+            NotifyPropertyChanged("Schedule");
+        }
+
+        public async Task GroupSelected(string value)
+        {
+            switch (value)
+            {
+                case null:
+                    return;
+                case "Load group...":
+                    AddGroupDialog dlg = new AddGroupDialog();
+                    await dlg.ShowAsync();
+                    if (dlg.Value == null) return;
+
+                    SelectedGroup = dlg.Value;
+                    break;
+                default:
+                    SelectedGroup = value;
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
